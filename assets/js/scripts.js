102b151d01773f8aee4f94c5bb6ab9f64f316e8b
yakoobkhedri <yakkobkhedrii@gmail.com>
2023-09-03 12:30:49 +0330

    commited

---
+++ assets/js/scripts.js
@@ -0,0 +1,83 @@
+
+// swiper
+var projects = new Swiper(".projects", {
+  effect: "coverflow",
+  loop:true,
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: 1,
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  breakpoints: {
+    992: {
+      slidesPerView: 3
+    },
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+var menuSwiper = new Swiper(".brands", {
+  loop: true,
+  autoplay: true,
+  slidesPerView: 2,
+  spaceBetween: 40,
+  breakpoints: {
+    1200: {
+      slidesPerView: 6
+    },
+    992: {
+      slidesPerView: 4
+    },
+    576: {
+      slidesPerView: 3
+    },
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    dynamicBullets: true,
+  },
+});
+// fancybox
+
+Fancybox.bind("[data-fancybox]", {
+  // Your custom options
+});
+// animation
+
+ScrollReveal().reveal('.top', {
+  duration: 2000,
+  origin: 'top',
+  distance: '100px',
+  delay: 500,
+  easing: 'ease-out'
+});
+
+ScrollReveal().reveal('.bottom', {
+  duration: 2000,
+  origin: 'bottom',
+  distance: '100px',
+  delay: 500,
+  easing: 'ease-out'
+});
+
+ScrollReveal().reveal('.left', {
+  duration: 2000,
+  origin: 'left',
+  distance: '100px',
+  delay: 500,
+  easing: 'ease-out'
+});
+
+ScrollReveal().reveal('.right', {
+  duration: 2000,
+  origin: 'right',
+  distance: '100px',
+  delay: 500,
+  easing: 'ease-out'
+});
